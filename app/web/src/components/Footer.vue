05e51e5c0fabfbd8ed5a41a8e5f09507f938fccd
jayachandralingam <31743008+jayachandralingam@users.noreply.github.com>
2018-03-04 10:13:07 -0500

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        CI CD testing with Kashti
+        CI CD testing 
       </div>
       <div class="col-lg-6">
       </div>
