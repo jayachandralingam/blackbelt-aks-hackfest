216aadcda4616a585da902d42a71d74868c45e32
jayachandralingam <31743008+jayachandralingam@users.noreply.github.com>
2018-03-14 15:14:31 -0400

    Update Footer.vue
    updated secrets

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        CI CD testing with bridage update bit ahead imagepullerror
+        CI CD testing with bridage updated secrets
       </div>
       <div class="col-lg-6">
       </div>
