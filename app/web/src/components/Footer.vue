832ce68848cd7f31d1cbab41b964bdea7949c9cc
jayachandralingam <31743008+jayachandralingam@users.noreply.github.com>
2018-03-14 14:13:03 -0400

    Update Footer.vue
    from MS comment #5 CI/CD issue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        CI CD testing 
+        CI CD testing with bridage update
       </div>
       <div class="col-lg-6">
       </div>
