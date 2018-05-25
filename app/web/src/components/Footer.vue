2b30846ca2aff93b6bd54d7d612c7df1b83d58e4
jayachandralingam <31743008+jayachandralingam@users.noreply.github.com>
2018-05-25 12:35:11 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        CI CD testing with bridage updated secrets
+        Jaya- CI CD testing with bridage updated secrets
       </div>
       <div class="col-lg-6">
       </div>
